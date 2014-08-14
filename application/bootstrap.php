7ea377a27e99cc107a6e49d2701cc20f145fa432
Bayurov <bayurovt@gmail.com>
2014-08-14 14:13:36 +0400

    fix

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -5,13 +5,13 @@
 
 // include APP_PATH.'/application/core/route.php';
 
+set_include_path(APP_PATH.'/application/');
+spl_autoload_extensions(".php");
+spl_autoload_register();
 
 include APP_PATH.'/application/core/route_map.php';
 include APP_PATH.'/application/lib/Application_helper.php';
 
-set_include_path(APP_PATH.'/application/');
-spl_autoload_extensions(".php");
-spl_autoload_register();
 
 Route::Start();
 
